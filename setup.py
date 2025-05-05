797291f8c76bbc7db8ec6ff074d00e8c404bce09
Amin <support@cybergraf.it>
2025-05-05 22:41:52 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="preventivi-cyberworks",
-    version="0.1.0",
+    version="0.1.2",
     description="CLI per generare e gestire preventivi in PDF",
     author="Amin",
     python_requires=">=3.10",
